b262db1bdb66824a037bbf8f0909e27b4f343926
rdx3334 <netflix.services24@gmail.com>
2024-11-18 15:29:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
- const statusMessages = ["Made By redaaa"];
+const statusMessages = ["Made By redaaa";"We are legends";"+1000 members"];
 
 
 let currentIndex = 0;
